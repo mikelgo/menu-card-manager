99c42d47894e27c016cc41612d59cf27568af5f9
michago <m.gonser@icloud.com>
2020-05-29 18:52:23 +0200

    feature: add shared module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,11 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+const MODULES = [CommonModule];
+
+@NgModule({
+  declarations: [],
+  imports: [...MODULES],
+  exports: [...MODULES]
+})
+export class SharedModule {}
