4ec6916de604f0ff5edda1a1df4f5718cbae477b
michago <m.gonser@icloud.com>
2020-05-29 19:59:41 +0200

    feature/header: add header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,25 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {RestaurantListModule} from './restaurant-list/restaurant-list.module';
+
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {HttpClientModule} from '@angular/common/http';
+import {RestaurantListModule} from './restaurant-list/restaurant-list.module';
+import {HeaderModule} from './header/header.module';
+import {SharedModule} from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, RestaurantListModule, FlexLayoutModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    RestaurantListModule,
+    FlexLayoutModule,
+    HttpClientModule,
+    HeaderModule,
+    SharedModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/header/header.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header.component';
+import {SharedModule} from '../shared/shared.module';
+
+
+
+@NgModule({
+  declarations: [HeaderComponent],
+  exports: [
+    HeaderComponent
+  ],
+  imports: [
+    SharedModule
+  ]
+})
+export class HeaderModule { }
